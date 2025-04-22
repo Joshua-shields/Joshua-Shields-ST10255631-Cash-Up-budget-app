348a5146990968c5a32200e9d69bcd4a9aeae4ae
kashc <kashc@THE_BEAST>
2025-04-22 19:51:33 +0200

    test push

--- app/src/main/java/com/example/cashup/LoginActivity.kt
+++ app/src/main/java/com/example/cashup/LoginActivity.kt
@@ -1,1 +1,25 @@
-// wagwan
+
+//***************************** start of login backend code **************************************//
+
+package com.example.cashup
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.widget.Button // allows for cliable buttons to be coded
+
+import android.widget.EditText // allows for text to be eddited  (input fields )
+
+import android.widget.Toast // needed for pop up messages
+
+class LoginActivity : AppCompatActivity() { // Needs to inherit from AppCompatActivity
+
+
+
+}
+
+
+
+
+//********************************** end of login backend code *****************************//
