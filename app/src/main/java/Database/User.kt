74b042d01299daa72351df5a89fd6460fcccfcb2
Joshua Dean Shields <shields.joshua23@gmail.com>
2025-04-22 20:25:41 +0200

    user table db

---
+++ app/src/main/java/Database/User.kt
@@ -0,0 +1,17 @@
+package Database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User(
+    // Primary Key
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val password: String,
+
+)
