2964dd6e2d593f2f6194cb3ab445f7e0c7fb49a5
kashc <kashc@THE_BEAST>
2025-04-23 22:19:00 +0200

    created a class called " ResetPasswordActivity", the goal is to add functionality to the "forgot password" option

---
+++ app/src/main/java/com/example/cashup/ResetPasswordActivity.kt
@@ -0,0 +1,20 @@
+package com.example.cashup
+
+//*************** start of imports ***************//
+
+    import android.content.Intent
+    import android.os.Bundle
+    import androidx.appcompat.app.AppCompatActivity
+    import android.widget.Button
+    import android.widget.EditText
+    import android.widget.Toast
+
+//******************* end of imports ****************//
+
+class ResetPasswordActivity {
+
+
+
+
+
+}
