085ac517ebfc96ea180fc2edf578cd329381516e
Joshua Dean Shields <shields.joshua23@gmail.com>
2025-04-24 13:06:10 +0200

    test

--- app/src/main/java/Database/UserDao.kt
+++ app/src/main/java/Database/UserDao.kt
@@ -1,9 +1,9 @@
 package com.example.cashup.Database
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import com.example.cashup.Database.User
-
 
 @Dao
 interface UserDao {
@@ -11,18 +11,19 @@ interface UserDao {
     @Query("SELECT * FROM user_table WHERE email = :email AND password = :password LIMIT 1")
     suspend fun findUserByCredentials(email: String, password: String): User?
 
-
-
     @Insert
     suspend fun insertUser(user: User)
-    fun getUserByEmail(email: String) {
 
-    }
+    // Removed the invalid getUserByEmail function
+
+    /*
+    // If you wanted a query to get user by email:
+    @Query("SELECT * FROM user_table WHERE email = :email LIMIT 1")
+    suspend fun getUserByEmail(email: String): User?
+    */
 
     /* @Insert
      suspend fun insertEmail(email : Email)
      */
 
-    
 }
-
