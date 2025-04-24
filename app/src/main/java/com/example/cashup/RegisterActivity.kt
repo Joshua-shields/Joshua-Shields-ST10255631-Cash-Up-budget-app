4a17ab1aeae8fc8e5c3b12b9efd37dca56ede24d
kashc <kashc@THE_BEAST>
2025-04-24 08:27:54 +0200

    created register page to handle logic for register ui actions. created local variables and included imports. functionality and code is still in progress

--- app/src/main/java/com/example/cashup/RegisterActivity.kt
+++ app/src/main/java/com/example/cashup/RegisterActivity.kt
@@ -1,4 +1,37 @@
 package com.example.cashup
 
-class RegisterActivity {
+//***************** Start of imports *****************************//
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+//******************* End of imports ****************************//
+
+
+class RegisterActivity
+{
+
+    //********* local variables *************//
+
+    private lateinit var nameEditText: EditText // needed for text input (name )
+
+    private lateinit var emailEditText: EditText // edit text for email
+
+    private lateinit var passwordEditText: EditText // edit text for password
+
+    private lateinit var confirmPasswordEditText: EditText // edit text for confirmation
+
+    private lateinit var registerButton: Button // register button functionality
+
+
+
+
+    //********* end of local variables ********//
+
 }
