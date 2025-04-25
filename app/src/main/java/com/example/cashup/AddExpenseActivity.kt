bd3f4c1b1f113c89bec14c5da3724f29521f846e
Darren-Stander <darrenjadestander@gmail.com>
2025-04-25 12:32:11 +0200

    test

--- app/src/main/java/com/example/cashup/AddExpenseActivity.kt
+++ app/src/main/java/com/example/cashup/AddExpenseActivity.kt
@@ -41,6 +41,7 @@ class AddExpenseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         // The XML layout
         setContentView(R.layout.activity_add_expense)
 
--- app/src/main/java/com/example/cashup/editCategoriesView.kt
+++ app/src/main/java/com/example/cashup/editCategoriesView.kt
@@ -152,4 +152,5 @@ class EditCategoriesView : AppCompatActivity() {
         finish()
     }
 }
+
 //------------------------------------------END OF FILE-------------------------------//
