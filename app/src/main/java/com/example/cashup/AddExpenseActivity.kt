497ccbc1fc561c80f3ed72168b7c5276bd164a20
Joshua Dean Shields <shields.joshua23@gmail.com>
2025-04-28 12:36:45 +0200

    test

--- app/src/main/java/com/example/cashup/AddExpenseActivity.kt
+++ app/src/main/java/com/example/cashup/AddExpenseActivity.kt
@@ -106,7 +106,6 @@ class AddExpenseActivity : AppCompatActivity() {
         amountInput = findViewById(R.id.amount_input)
         categoriesButton = findViewById(R.id.categories_button)
         dateInput = findViewById(R.id.date_input)
-        noteInput = findViewById(R.id.note_input)
         attachReceiptButton = findViewById(R.id.attach_file_button)
         createExpenseButton = findViewById(R.id.create_expense_button)
 
